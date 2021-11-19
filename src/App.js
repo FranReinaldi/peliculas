638d4618f69bdf8a5d5b9d69aba7121fd8d7e700
Tincho <franco.reinaldi@hotmail.com>
2021-11-19 14:37:39 -0300

    hola

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import Pelicula from './pelicula';
+import PageWrapper from './PageWrapper';
+import peliculasJson from './peliculas.json';
+import Paginacion from './paginacion';
+import { useState } from 'react';
 
 function App() {
+
+  const [paginaActual, setPaginaActual] = useState(1);
+  const totalPorPagina = 5;
+
+  let peliculas = peliculasJson;
+
+  const buscarPeliculas = async () => {
+    let url ='https://cors-anywhere.herokuapp.com/https://lucasmoy.dev/data/react/peliculas.json';
+    
+    
+    let respuesta = await fetch(url, {
+      "method": 'GET',
+      "headers": {
+        "Accept": 'application/json',
+        "Content-Type": 'application/json',
+        "Origin": 'https://lucasmoy.dev/data/react/peliculas.json'
+      }
+    });
+    let json = await respuesta.json();
+    debugger;
+  }
+
+  buscarPeliculas();
+
+  const cargarPeliculas = () => {
+    peliculas = peliculas.slice(
+      (paginaActual - 1) * totalPorPagina ,
+      paginaActual * totalPorPagina
+    );
+  }
+ 
+
+  const getTotalPaginas = () =>{
+    let cantidadTotalPeliculas = peliculasJson.length;
+    return(
+      Math.ceil(cantidadTotalPeliculas / totalPorPagina)
+      );
+  }
+
+  cargarPeliculas();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageWrapper>
+
+      {peliculas.map(pelicula =>
+        <Pelicula titulo={pelicula.titulo} calificacion={pelicula.calificacion} img={pelicula.img}
+          runtime={pelicula.runtime} pg={pelicula.pg} release={pelicula.release} director={pelicula.director} actores={pelicula.actores} >
+          {pelicula.descripcion}
+        </Pelicula>
+
+      )}
+      <Paginacion pagina={paginaActual} total={getTotalPaginas()} onChange={(pagina) => {
+        setPaginaActual(pagina)
+      }} />
+
+    </PageWrapper>
   );
 }
 
